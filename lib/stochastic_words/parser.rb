9a3a0df7271ad066ca4271fee0a865fe9f90bf11
Paul Niezborala <pauniezbo@gmail.com>
2023-02-17 23:25:18 +0100

    Support weighting providers

--- lib/stochastic_words/parser.rb
+++ lib/stochastic_words/parser.rb
@@ -9,34 +9,34 @@ module StochasticWords
       @gram_size = gram_size
     end
 
-    def parse(provider)
+    def parse(provider, weight: 1)
       provider.each_word do |word, frequency|
         state = [ INITIAL_CHARACTER ] * gram_size
 
         (0..word.length() - 1).each do |character_index|
           current_character_value = word[character_index]
 
-          count_association(state, current_character_value, frequency)
+          count_association(state, current_character_value, weight * frequency)
 
           # Change state to move to the next character
           state.push(current_character_value).shift
         end
 
         # Finish the word
-        count_association(state, FINISHING_CHARACTER, frequency)
+        count_association(state, FINISHING_CHARACTER, weight * frequency)
       end
     end
 
     alias_method :<<, :parse
 
-    def count_association(state, character_value, frequency)
+    def count_association(state, character_value, weight)
       # Find or create the appropriate entries in the database
       n_gram = NGram.find_or_create_by!(items: state)
       character = Character.find_or_create_by!(value: character_value)
 
       association = NGramCharacterAssociation.find_or_create_by!(n_gram: n_gram, character: character)
       puts association.inspect
-      association.increment!(:count, frequency)
+      association.increment!(:count, weight)
     end
   end
 end
